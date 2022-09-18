9f9d9c116b3745a607d69f7192bf01784799b738
Mai Quốc Bảo <104915677+baoq06@users.noreply.github.com>
2022-09-18 21:48:10 +0700

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -12,9 +12,9 @@ template<typename T> bool minimize(T& a, T b) {return (a > b ? a = b, 1 : 0);}
 const long INF = ~(1LL << 63);
 const int MOD = 1e9 + 7;
 
-long add_mod(long x, long y) {x += y; if(x >= MOD) return x - MOD; return x;}
-long sub_mod(long x, long y) {x -= y; x %= MOD; if(x < 0) x += MOD; return x;}
-long mul_mod(const long &x, const long &y) {return(1LL * ((x % MOD) * (y % MOD) % MOD));}
+long add(long x, long y) {x += y; if(x >= MOD) return x - MOD; return x;}
+long sub(long x, long y) {x -= y; x %= MOD; if(x < 0) x += MOD; return x;}
+long mul(const long &x, const long &y) {return(1LL * ((x % MOD) * (y % MOD) % MOD));}
 
 void setIO(string NAME = "")
 {
