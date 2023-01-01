23c1c2d8b428db1f9bb253bd2ba03a36b75f0b96
Mai Quốc Bảo <104915677+baoq06@users.noreply.github.com>
2023-01-01 19:07:07 +0700

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -2,18 +2,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define long long long
+#define ll long long
 #define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
 
 template<typename T> bool maximize(T& a, T b) {return (a < b ? a = b, 1 : 0);}
 template<typename T> bool minimize(T& a, T b) {return (a > b ? a = b, 1 : 0);}
 
-const long INF = ~(1LL << 63);
+const ll INF = ~(1LL << 63);
 const int MOD = 1e9 + 7;
 
-long add(long x, long y) {x += y; if(x >= MOD) return x - MOD; return x;}
-long sub(long x, long y) {x -= y; x %= MOD; if(x < 0) x += MOD; return x;}
-long mul(const long &x, const long &y) {return(1LL * ((x % MOD) * (y % MOD) % MOD));}
+ll add(ll x, ll y) {x += y; if(x >= MOD) return x - MOD; return x;}
+ll sub(ll x, ll y) {x -= y; x %= MOD; if(x < 0) x += MOD; return x;}
+ll mul(const ll &x, const ll &y) {return(1LL * ((x % MOD) * (y % MOD) % MOD));}
 
 void setIO(string NAME = "")
 {
@@ -26,6 +27,8 @@ signed main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+
     
     return 0;
 }
