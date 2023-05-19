216fbd28cf5c1afe73ee848256b6a22bc48ad820
Mai Quốc Bảo <104915677+baoq06@users.noreply.github.com>
2023-05-19 20:13:48 +0700

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,4 +1,4 @@
-//thao nhu my beloved <3
+//The code below is written by this dude -> baoq06
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -9,20 +9,17 @@ using namespace std;
 template<class X, class Y> bool maximize(X &a, const Y& b) {X eps = 1e-9; return (b > a + eps) ? a = b, 1 : 0;}
 template<class X, class Y> bool minimize(X &a, const Y& b) {X eps = 1e-9; return (b + eps < a) ? a = b, 1 : 0;}
 
-const ll INF = ~(1LL << 63);
-const int MOD = 1e9 + 7;
-
 void setIO(string NAME = "")
 {
-    if((int) NAME.length() && fopen((NAME + ".inp").c_str(), "r"))
-    freopen((NAME + ".inp").c_str(), "r", stdin),
-    freopen((NAME + ".out").c_str(), "w", stdout);
+	if((int) NAME.length() && fopen((NAME + ".inp").c_str(), "r"))
+	freopen((NAME + ".inp").c_str(), "r", stdin),
+	freopen((NAME + ".out").c_str(), "w", stdout);
 }
 
 int main() 
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-    return 0;
+	return 0;
 }
