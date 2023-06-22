3b2343aa793ed065dfcb2f577420cae3710898f4
Mai Quốc Bảo <104915677+baoq06@users.noreply.github.com>
2023-06-22 19:23:30 +0700

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,4 +1,4 @@
-//The code below is written by this dude -> baoq06
+//baoq06 - Uoc mo tuong lai lam thu tuong Hoa Ki
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -11,15 +11,15 @@ template<class X, class Y> bool minimize(X &a, const Y& b) {X eps = 1e-9; return
 
 void setIO(string NAME = "")
 {
-	if((int) NAME.length() && fopen((NAME + ".inp").c_str(), "r"))
-	freopen((NAME + ".inp").c_str(), "r", stdin),
-	freopen((NAME + ".out").c_str(), "w", stdout);
+    if((int) NAME.length() && fopen((NAME + ".inp").c_str(), "r"))
+    freopen((NAME + ".inp").c_str(), "r", stdin),
+    freopen((NAME + ".out").c_str(), "w", stdout);
 }
 
 int main() 
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	return 0;
+    return 0;
 }
