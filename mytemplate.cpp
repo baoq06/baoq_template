66323f673fcfd9849f4737026f60980dd2222f6a
Mai Quốc Bảo <104915677+baoq06@users.noreply.github.com>
2024-09-12 06:12:11 +0700

    Update mytemplate.cpp

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,4 +1,3 @@
-//baoq06 - Uoc mo tuong lai lam thu tuong Hoa Ki
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -9,17 +8,13 @@ using namespace std;
 template<class X, class Y> bool maximize(X &a, const Y& b) {X eps = 1e-9; return (b > a + eps) ? a = b, 1 : 0;}
 template<class X, class Y> bool minimize(X &a, const Y& b) {X eps = 1e-9; return (b + eps < a) ? a = b, 1 : 0;}
 
-void setIO(string NAME = "")
-{
-    if((int) NAME.length() && fopen((NAME + ".inp").c_str(), "r"))
-    freopen((NAME + ".inp").c_str(), "r", stdin),
-    freopen((NAME + ".out").c_str(), "w", stdout);
+void setIO(string NAME = "") {
+	if((int) NAME.length() && fopen((NAME + ".inp").c_str(), "r"))
+	freopen((NAME + ".inp").c_str(), "r", stdin),
+	freopen((NAME + ".out").c_str(), "w", stdout);
 }
 
-int main() 
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+int main() {
 
-    return 0;
+	return 0;
 }
